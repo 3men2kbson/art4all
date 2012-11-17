5d9cb934d8488d33fa2fde5ec19235eddc29e2ba
yamit <yamidcardenas@gmail.com>
2012-11-17 02:46:37 -0500

    separated classes

---
+++ model/user.py
@@ -0,0 +1,23 @@
+import os
+
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask
+
+__author__ = 'yamit'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
